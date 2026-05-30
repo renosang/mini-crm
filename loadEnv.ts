a1b2dc0000395e08024d023461b353c4abd0559c
renosang <renosang@gmail.com>
2026-05-31 02:10:28 +0700

    Fix: support loading .env file on VPS

--- loadEnv.ts
+++ loadEnv.ts
@@ -1,2 +1,15 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local', override: true });
+import fs from 'fs';
+import path from 'path';
+
+const envLocalPath = path.resolve(process.cwd(), '.env.local');
+const envPath = path.resolve(process.cwd(), '.env');
+
+if (fs.existsSync(envLocalPath)) {
+  dotenv.config({ path: envLocalPath, override: true });
+} else if (fs.existsSync(envPath)) {
+  dotenv.config({ path: envPath, override: true });
+} else {
+  dotenv.config();
+}
+
